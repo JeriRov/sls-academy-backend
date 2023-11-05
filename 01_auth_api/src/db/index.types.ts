238f95e2aea79b8af142608c1a99073a0c7927ae
Artem Mahey <artemmagej@gmail.com>
2023-11-05 22:52:37 +0200

    feat: make QueryValues more reusable

--- 01_auth_api/src/db/index.types.ts
+++ 01_auth_api/src/db/index.types.ts
@@ -1,4 +1,7 @@
-export type QueryValues = Array<string | number | boolean | null | Date>;
+export type QueryValue = string | number | boolean | null | Date;
+export type QueryValues = Array<QueryValue>;
+
+
 export type CheckConnectionResult = {
     time: Date | null;
 }
