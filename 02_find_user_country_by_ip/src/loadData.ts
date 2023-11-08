81c12f5c9881a7cbb288debb59477765da98ef63
Artem Mahey <artemmagej@gmail.com>
2023-11-08 13:57:36 +0200

    feat: use reduce for load ip ranges

--- 02_find_user_country_by_ip/src/loadData.ts
+++ 02_find_user_country_by_ip/src/loadData.ts
@@ -7,19 +7,22 @@ export type IPRange = {
     countryName: string;
 }
 
+const formatRange = (ipPart: string, leftBorder: number = 1, rightBorder: number = -1) => ipPart.trim().slice(leftBorder, rightBorder);
+
+
 export const loadIPRanges = () => {
     const lines = fs.readFileSync('IP2LOCATION-LITE-DB1.CSV', 'utf8').split('\n');
-    const ipRanges: IPRange[] = new Array(lines.length);
-    for (let i = 0; i < lines.length; i++) {
-        if (lines[i].length > 0) {
-            let parts = lines[i].split(',');
-            ipRanges[i] = {
-                start: Number(parts[0].trim().slice(1, -1)),
-                end: Number(parts[1].trim().slice(1, -1)),
-                countryCode: parts[2].trim().slice(1, -1),
-                countryName: parts[3].trim().slice(1, -1)
-            }
+
+    return lines.reduce((ipRanges, line) => {
+        if (line.length > 0) {
+            const parts = line.split(',');
+            ipRanges.push({
+                start: Number(formatRange(parts[0])),
+                end: Number(formatRange(parts[1])),
+                countryCode: formatRange(parts[2]),
+                countryName: formatRange(parts[3]),
+            });
         }
-    }
-    return ipRanges;
-}
+        return ipRanges;
+    }, [] as IPRange[]);
+};
